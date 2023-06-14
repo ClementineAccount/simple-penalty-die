5b5d859474caa889ab4ab1f74d1814d71988a671
ClementineAccount <ClementineShamaney@gmail.com>
2023-06-14 14:56:01 +0800

    Refactors addPenaltyDie

--- scripts/penalty-die.mjs
+++ scripts/penalty-die.mjs
@@ -1,38 +1,58 @@
 /** 
-* Calls hook on an event before an ability roll is made, after the dice pool is assembled, in 
-* order to add a penalty dice to the dice roll that takes in account the exhaustion level of the character
-* https://github.com/foundryvtt/dnd5e/wiki/Hooks
-* 
-* @param {Actor5e} actor - Actor for which the ability test is being rolled. Used here to retrieve the exhaustion level
-* @param {D20RollConfiguration} rollData - Configuration data for the pending roll. Used here to add the additional die
+* Add the penalty dice to the dice roll that takes in account the exhaustion level of the character.
+* @param {number} exhaustionLevel - The exhaustion level that was passed in
+* @param {D20RollConfiguration} rollData - Configuration data for the pending roll. Used here to add the additional die.
 */
-Hooks.on("dnd5e.preRollAbilityTest", (actor, rollData) => {
-
-  //To Do: Clean up this stuff and move it to its own function
+function addPenaltyDie(exhaustionLevel, rollData) {
   //Using just if/else because maximum possible exhaustion level is 6 so don't need to map it.
-  if (actor.system.attributes.exhaustion == 1)
+  if (exhaustionLevel == 1)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d4";
   }
-  else if (actor.system.attributes.exhaustion == 2)
+  else if (exhaustionLevel == 2)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d6";
   }
-  else if (actor.system.attributes.exhaustion == 3)
+  else if (exhaustionLevel == 3)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d8";
   }
-  else if (actor.system.attributes.exhaustion == 4)
+  else if (exhaustionLevel == 4)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d10";
   }
-  else if (actor.system.attributes.exhaustion == 5)
+  else if (exhaustionLevel == 5)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d12";
   }
+};
+
+/* -------------------------------------------- */
+/*  Hooks                                       */
+/* -------------------------------------------- */
+/*List of dnd5e system hooks used: https://github.com/foundryvtt/dnd5e/wiki/Hooks*/
+
+/** 
+* Calls hook on an event before an ability check roll is made
+*
+* @param {Actor5e} actor - Actor for which the ability test is being rolled. Used here to retrieve the exhaustion level
+* @param {D20RollConfiguration} rollData - Configuration data for the pending roll. Used here to add the additional die
+*/
+Hooks.on("dnd5e.preRollAbilityTest", (actor, rollData) => {
+  addPenaltyDie(actor.system.attributes.exhaustion, rollData);
+});
+
+/** 
+* Calls hook on an event before an ability saving throw's roll is made
+*
+* @param {Actor5e} actor - Actor for which the ability test is being rolled. Used here to retrieve the exhaustion level
+* @param {D20RollConfiguration} rollData - Configuration data for the pending roll. Used here to add the additional die
+*/
+Hooks.on("dnd5e.preRollAbilitySave", (actor, rollData) => {
+  addPenaltyDie(actor.system.attributes.exhaustion, rollData);
 });
