eb2ba385e17b44011d745786099c343355e372cb
ClementineAccount <ClementineShamaney@gmail.com>
2023-06-14 14:38:55 +0800

    Adds documentation and else checks

--- scripts/penalty-die.mjs
+++ scripts/penalty-die.mjs
@@ -1,79 +1,38 @@
-Hooks.on("dnd5e.preRollSkill", (actor, rollData, skillId) => {
-  console.log("dnd5e.preRollSkill");
-  console.log(rollData);
+/** 
+* Calls hook on an event before an ability roll is made, after the dice pool is assembled, in 
+* order to add a penalty dice to the dice roll that takes in account the exhaustion level of the character
+* https://github.com/foundryvtt/dnd5e/wiki/Hooks
+* 
+* @param {Actor5e} actor - Actor for which the ability test is being rolled. Used here to retrieve the exhaustion level
+* @param {D20RollConfiguration} rollData - Configuration data for the pending roll. Used here to add the additional die
+*/
+Hooks.on("dnd5e.preRollAbilityTest", (actor, rollData) => {
 
   //To Do: Clean up this stuff and move it to its own function
-  console.log(actor.system.attributes.exhaustion);
+  //Using just if/else because maximum possible exhaustion level is 6 so don't need to map it.
   if (actor.system.attributes.exhaustion == 1)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d4";
   }
-
-  if (actor.system.attributes.exhaustion == 2)
+  else if (actor.system.attributes.exhaustion == 2)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d6";
   }
-
-  if (actor.system.attributes.exhaustion == 3)
+  else if (actor.system.attributes.exhaustion == 3)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d8";
   }
-
-  if (actor.system.attributes.exhaustion == 4)
+  else if (actor.system.attributes.exhaustion == 4)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d10";
   }
-  
-  if (actor.system.attributes.exhaustion == 5)
+  else if (actor.system.attributes.exhaustion == 5)
   {
     rollData.parts.push("@penalty");
     rollData.data.penalty = "-1d12";
   }
-
-  console.log(rollData);
-});
-
-
-Hooks.on("dnd5e.preRollAbilityTest", (actor, rollData, skillId) => {
-  //console.log("dnd5e.preRollAbilityTest");
-  //console.log(rollData);
-
-  //console.log(actor.system.attributes.exhaustion);
-  
-    //To Do: Clean up this stuff and move it to its own function
-  if (actor.system.attributes.exhaustion == 1)
-  {
-    rollData.parts.push("@penalty");
-    rollData.data.penalty = "-1d4";
-  }
-
-  if (actor.system.attributes.exhaustion == 2)
-  {
-    rollData.parts.push("@penalty");
-    rollData.data.penalty = "-1d6";
-  }
-
-  if (actor.system.attributes.exhaustion == 3)
-  {
-    rollData.parts.push("@penalty");
-    rollData.data.penalty = "-1d8";
-  }
-
-  if (actor.system.attributes.exhaustion == 4)
-  {
-    rollData.parts.push("@penalty");
-    rollData.data.penalty = "-1d10";
-  }
-  
-  if (actor.system.attributes.exhaustion == 5)
-  {
-    rollData.parts.push("@penalty");
-    rollData.data.penalty = "-1d12";
-  }
-
-  //console.log(rollData);
 });
